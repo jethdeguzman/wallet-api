79b4aac0e6992182424333e3fbf59c9ff963fce7
Jethro De Guzman <jethro@acudeen.com>
2018-09-22 19:43:33 +0800

    Import api request objects on module init
    Signed-off-by: Jethro De Guzman <jethro@acudeen.com>

---
+++ wallet/api/__init__.py
@@ -0,0 +1,12 @@
+from .account import CreateAccountRequest, LogInAccountRequest
+from .wallet import CreateWalletRequest, GetWalletsRequest, GetWalletRequest
+from .transaction import GetTransactionsRequest
+
+__all__ = [
+    CreateAccountRequest,
+    LogInAccountRequest,
+    CreateWalletRequest,
+    GetWalletsRequest,
+    GetWalletRequest,
+    GetTransactionsRequest
+]
